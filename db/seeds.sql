dd0be25e3842c02e62ea6e19b33b77d6604c893a
andrew-brown <andrewmbrown97@gmail.com>
2024-10-15 17:21:30 -0400

    adding dotenv

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,34 @@
--- Inserts names of departments into department table
-INSERT INTO department
-  (name)
-VALUES
-  ('Engineering'),
-  ('Sales'),
-  ('Finance'),
-  ('Legal');
+INSERT INTO department (name)
+VALUES  ("Engineering"),
+        ("Finance"),
+        ("Interconnected"),
+        ("Sales"),
+        ("Legal");
 
--- Inserts roles of employee into role table
-INSERT INTO role
-  (title, salary, department_id)
-VALUES
-  ('Software Engineer', 85000, 1),
-  ('Salesperson', 75000, 2),
-  ('Accountant', 125000, 3),
-  ('Lawyer', 200000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Engineering Manager", 120000, 1),
+        ("Engineering Lead", 100000, 1),
+        ("Staff Engineer", 80000, 1),
+        ("Finance Manager", 85000, 2),
+        ("Accountant", 70000, 2),
+        ("Brand Advocate Manager", 95000, 3),
+        ("Brand Advocate Sr. Analyst", 82000, 3),
+        ("Brand Advocate Analyst", 70000, 3),
+        ("Sales Manager", 75000, 4),
+        ("Salesperson", 65000, 4),
+        ("Legal Manager", 110000, 5),
+        ("Lawyer", 95000, 5);
 
--- Inserts employee information into employee table
-INSERT INTO employee
-  (first_name, last_name, role_id, manager_id)
-VALUES
-  ('Juan', 'Garcia', 1, 4),
-  ('Jonathan', 'Villcapoma', 2, 3),
-  ('Jesus', 'Meraz', 3, 1),
-  ('Estefany', 'Munoz', 4, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Roxy", "Say",  1, NULL),
+        ("Jane", "Doe",  2, 1),
+        ("Janet", "Doe",  3, 1), 
+        ("Jennifer", "Doe",  4, NULL), 
+        ("Julia", "Doe",  5, 4),
+        ("Jaya", "Dave",  6, NULL), 
+        ("Janki", "Dave",  7, 6), 
+        ("Juhi", "Dave",  8, 6), 
+        ("Jin", "Dai",  9, NULL), 
+        ("Jooeun", "Dai",  10, 9), 
+        ("Jocelyn", "Davalos",  11, NULL), 
+        ("Jaylia", "Davalos",  12, 11);
